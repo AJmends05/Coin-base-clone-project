ccbef6f325a6f48248a6134134b25077d0c5498a
AJmends05 <AJmends05-jmendsbrew@gmail.com>
2026-05-02 12:35:29 +0000

    Add Netlify URL to CORS

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:3000"],
+  origin: ["http://localhost:5173", "http://localhost:3000", "https://jmends-crypto-app.netlify.app"],
   credentials: true,
 }));
 app.use(express.json());
